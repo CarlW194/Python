b2ef9bb8bf754089164f873d6522403c11319e17
CarlW194 <carl.white194@gmail.com>
2014-12-05 18:11:14 +0000

    ListExample.py
    This file is an example of how to use Lists in python and how to print out the desired values.

---
+++ ListExample.py
@@ -0,0 +1,9 @@
+list = ['abcd' , 786, 2.23, 'john', 70.2]
+tinyList = [123, 'john']
+
+print list	#prints complete list
+print list[0]	#print first element of list
+print list[1:3]	#prints elements starting from 2nd till 3rd
+print list[2:]	#prints list from 3rd position
+print tinyList*2	#prints out list twice
+print list + tinyList	#prints concatenated lists	
