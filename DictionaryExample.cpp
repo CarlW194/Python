11fef4db08a91571edd9ca678c0b19e3563bc3a9
CarlW194 <carl.white194@gmail.com>
2014-12-05 17:47:40 +0000

    DictionaryExample.cpp
    This file shows how to create a dictionary of items in python and how to initialise it and print out the desired results from it.

---
+++ DictionaryExample.cpp
@@ -0,0 +1,13 @@
+#dictionaries are like hash tables(key value pairs)
+
+dict = {}
+dict['one'] = "This is one"
+dict[2]	= "This is two"
+
+tinyDict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+print dict['one']	#prints the value for 'one' key
+print dict[2]		#prints value for 2 key
+print tinyDict		#prints complete dictionary
+print tinyDict.keys()	#prints all the keys
+print tinyDict.values() #prints all the values	
