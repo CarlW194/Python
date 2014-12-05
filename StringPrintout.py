89f79d695babdb4889ffd00713fd78dd7632a930
CarlW194 <carl.white194@gmail.com>
2014-12-05 18:55:04 +0000

    StringPrintout.py
    This file shows how to printout a string to the screen many different ways depending on the desired result.

---
+++ StringPrintout.py
@@ -0,0 +1,8 @@
+str = 'Hello World!'
+
+print str	#prints whole tring
+print str[0]	#prints the first character
+print str[2:5]	#prints characters at 3rd to last position
+print str[2:]	#prints all characters from 3rd position onwards
+print str + "Test"	#prints out string concatenated by TEST
+ 
