abfc642d0d561716166158f8e2f729597cf0d891
CarlW194 <carl.white194@gmail.com>
2014-12-05 18:07:14 +0000

    InputFunction.py
    This file defines a function which takes in a string entered by the user and prints the appropriate messages to the screen. This shows how python uses input to the screen.

---
+++ InputFunction.py
@@ -0,0 +1,7 @@
+def printme(str):
+	"This prints a passed String into this function"
+	print str;
+	return;
+
+printme("I'm first call to user defined funtion!");
+printme("Again Secong to call to the same Funtion");
