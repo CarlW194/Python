ae26f8fc3ddcf5835ff8edc95c06b4881c3b6e00
CarlW194 <carl.white194@gmail.com>
2014-12-05 17:43:37 +0000

    BitwiseOperations.cpp
    This file is an example of how python uses bitwise operations and prints them to the screen.

---
+++ BitwiseOperations.cpp
@@ -0,0 +1,23 @@
+#bitwise operations
+
+a=60
+b=13
+c=0
+
+c=a&b;
+print "Line 1 - value of c is ", c
+
+c=a|b
+print "Line 2 - value of c is ", c
+
+c=a^b
+print "Line 3 - value of c is ", c
+
+c=~a
+print "Line 4 - value of c is ", c
+
+c=a<<2
+print "Line 5 - value of c is ", c
+
+c=a>>2;
+print "Line 6 - value of c is ", c
